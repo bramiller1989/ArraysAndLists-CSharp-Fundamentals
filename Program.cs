bd673ccd23cf8e8f24429f14a40afeb02883bb3f
bramiller1989 <bramiller1989@gmail.com>
2021-01-20 16:07:33 -0600

    add exercise2

--- Program.cs
+++ Program.cs
@@ -29,10 +29,24 @@ namespace ArraysAndLists
                 Console.WriteLine();
         }
 
+        public void Exercise2()
+        {
+            Console.Write("What's your name? ");
+            var name = Console.ReadLine();
+
+            var array = new char[name.Length];
+            for (var i = name.Length; i > 0; i--)
+                array[name.Length - i] = name[i - 1];
+
+            var reversed = new string(array);
+            Console.WriteLine("Reversed name: " + reversed);
+        }
+
         static void Main(string[] args)
         {
             Program program = new Program();
             program.Exercise1();
+            program.Exercise2();
         }
     }
 }
